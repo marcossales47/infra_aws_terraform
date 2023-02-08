d954ce3104db49b4c08bd802d46e7cdd30f4ddb5
marcossales47 <sales.marcos10@gmail.com>
2023-02-08 11:03:15 -0300

    commit

---
+++ resources.tf
@@ -0,0 +1,49 @@
+resource "aws_instance" "site_dev"{
+  ami = var.ami_aws #para o auto-scalling coloca-se a AMI template criada para isso.
+  instance_type = "t2.micro"
+  key_name = "aws_marcos" #Aqui coloca-se o nome da key
+  tags = {
+    name = "teste"
+  }
+  vpc_security_group_ids = ["${aws_security_group.block_public_access.id}"] #Id do Security Group que libera apenas o ssh
+}
+
+# criação do bucket S3 público.
+
+resource "aws_s3_bucket" "ti_saude_s3" {
+  bucket = var.s3_bucket_name
+}
+
+#Criação do RDS com Mysql
+resource "aws_db_instance" "ti_saude_rds" {
+  allocated_storage    = 10
+  db_name              = "ti_saude_db"
+  engine               = "mysql"
+  engine_version       = "5.7"
+  instance_class       = "t2.micro"
+  username             = "teste"
+  password             = "ti_saude"
+  parameter_group_name = "default.mysql5.7"
+
+  vpc_security_group_ids = ["${aws_security_group.block_public_access.id}"] #Id do Security Group que libera apenas o ssh
+  
+}
+
+#criação de Security Group para barrar Acesso exter à ec2 do site
+resource "aws_security_group" "block_public_access" {
+  name        = "allow ssh"
+  description = "Bloqueia acesso publico"
+  vpc_id = "vpc-0609a528f0c962b35"
+
+  ingress {
+    from_port        = 22
+    to_port          = 22
+    protocol         = "tcp"
+
+    cidr_blocks      = ["177.19.149.146/32"] #só aceita conexão ssh pelos ip's liberados
+    
+  }
+  tags = {
+    Name = "block_public_access"
+  }
+}
---
+++ variables.tf
@@ -0,0 +1,12 @@
+variable "ami_aws" {
+  default = "ami-0aa7d40eeae50c9a9"
+}
+
+variable "s3_bucket_name" {
+  default = "TesteDevOps"
+}
+
+variable "regiao_aws" {
+  default = "us-east-1"
+}
+
